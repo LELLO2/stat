ba41ecafac8f433c4577fb6ef87a6a3c6510a553
LELLO2 <114951822+LELLO2@users.noreply.github.com>
2022-10-04 16:35:32 +0200

    Add files via upload

---
+++ homeworkvb/WinFormsApp2/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        RichTextBox1.Text = "1. Create a simple applications in c# and vbnet to play with handlers and to understand sintax differences betweeen them two"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RichTextBox1.Text = "1. Definition of statistics
+2 What is a dataset? From the observation units to the dataset attributes and values  
+3 Find interesting application of statistics in cybersecurity."
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        RichTextBox1.Text = "1. main differences between c and vbnet"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
